f49e93ca065c30b2dcc5699455506bab5ffc177d
Monica <mntamaru@gmail.com>
2019-09-06 12:32:44 -0700

    add logic for active tab

--- src/containers/TabbedCard.js
+++ src/containers/TabbedCard.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react'
 
-class CardSlider extends Component {
+var cns = require("classnames");
 
+class TabbedCard extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state {current_index: this.props.index || 0};
+
+	}
 
 	render () {
 
+		// active class when the TabbedCard index matches the tab index
+		let tabs = _.map(this.props.children, function (child, index) {
+      var className = cns({'active': self.state.current_index === index});
+
+    this.handleClick = (index) => {
+	    this.setState({
+	      current_index: index
+	    });
+  	}
 
 		return(
-				< InfoCard />
+				<li onClick={self.handleClick}>
+          <a className={className} href="#">{child.props.display}</a>
+        </li>
 
 		)
 	}
