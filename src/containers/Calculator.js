dee2f3204f96899d2b19394d8499154d16610659
Monica <mntamaru@gmail.com>
2019-09-13 16:45:38 -0700

    updated styling and added tab content

---
+++ src/containers/Calculator.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Should I open a 529?
+          </h1>
+        </header>
+
+        <body>
+          < TabbedCardContainer />
+        </body>
+      </div>
+    );
+  } 
+}
+
+export default App;
--- src/containers/TabbedCardContainer.js
+++ src/containers/TabbedCardContainer.js
@@ -16,12 +16,38 @@ class TabbedCardContainer extends Component {
         <Tabs selectedIndex={this.state.tabIndex} 
         onSelect={tabIndex => this.setState({ tabIndex })}>
           <TabList>
-            <Tab>What?</Tab>
-            <Tab>Why?</Tab>
+            <Tab>What is a 529 Plan?</Tab>
+            <Tab>Why would I want to use a 529?</Tab>
+            <Tab>Potential Concerns?</Tab>
           </TabList>
-          <TabPanel>A 529 is a...</TabPanel>
-          <TabPanel>A 529 is a tax advantaged...</TabPanel>
+          <TabPanel>
+            <p>A 529 is a tax-advantaged investment 
+            vehicle in the US designed for encouraging saving for 
+            the future higher education expenses of a designated 
+            beneficiary.</p>
+
+            <p>You can use a 529 to pay for K-12 public, private and 
+            religios school tuition, post-secondary education costs, such as
+            tuition, fees, books, supplies & equipment, room and board.*</p>
+          </TabPanel>
+          <TabPanel>
+            <p>With the Economic Growth & Tax Relief Reconciliation
+            Act of 2001, qualified distributions from 529 plans are exempt 
+            from federal income tax.</p>
+          </TabPanel>
+          <TabPanel>
+            <ul>
+              <li>If the money is not spent on education, expenses are subject to income tax, 
+              plus a 10% penalty.</li>
+              <li>Paying qualified expenses directly from a 529 may reduce a student's 
+              eligibility for need based financial aid.</li>
+              <li>Paying qualified expenses from a 529 may reduce eligibility for the 
+              American Opportunities Tax Credit, due to IRS coordination restrictions.</li>
+            </ul>
+            *
+          </TabPanel>
         </Tabs>
+        <div><p> * check your individual state rules for what a 529 can cover, as it varies.</p></div>
       </div>
     );
   }
