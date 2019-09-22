7dfc0a3820f136da5386cf93796321e9d998b305
Monica <mntamaru@gmail.com>
2019-09-21 18:15:02 -0700

    add field for calculator form

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,26 +2,39 @@ import React, { Component } from 'react';
 import './style/Calculator.css';
 
 
+// inital investment + annual investment, with annual rate calculated yearly
 function displayFutureValue() {
 
 	// Get user input from the text field that has the id.
 	var principal = parseFloat(document.getElementById('principal').value);
 	var annualRate = parseFloat(document.getElementById('annualRate').value);
+	var annualInvestment = parseFloat(document.getElementById('annualInvestment').value);
 	var years = parseFloat(document.getElementById('years').value);
 	
 	// Call the function computeFutureValue(principal, annualRate, years)
 	// and store the value returned by function in a variable
-	var futureValue = computeFutureValue(principal, annualRate, years);
+	var futureValue = computeFutureValue(principal, annualInvestment, annualRate, years);
 	
 	// Display the result
 	document.getElementById('outputDiv').innerHTML = 'Future Value is $ ' + futureValue;
 }
  
 
-function computeFutureValue(principal, annualRate, years) {
+function computeFutureValue(principal, annualInvestment, annualRate, years) {
 
+	let f = 0;
+	let hold
+	let jan = principal * annualRate;
 	// Compute and return the future value of an investment.
-	var f = principal * Math.pow(1 + annualRate, years);
+	for (let i=0; i<years; i++){
+
+		// my math is wrong, but the idea is ok
+		hold = jan + annualInvestment;
+
+		f = (f + hold) * (1 + annualRate);
+		
+	}
+	
  
  	console.log(f)
 	return f.toFixed(2);
@@ -54,6 +67,8 @@ class Calculator extends Component {
 				<form id="investment" onSubmit={this.handleOnSubmit}>
 					<label>Initial Investment (principal)</label> 
 					<input type="text" id="principal"/><br/> <br/>
+					<label>Annual Investment (yearly payments)</label> 
+					<input type="text" id="annualInvestment"/><br/> <br/>
 					<label>Annual rate (example: .065)</label> 
 					<input type="text" id="annualRate" /> <br/><br/>
 					<label>Number of years</label>
